c116a858b8358a9cabef8979dbe2cc96c8f4c735
Frank Agerholm <fae@netuse.de>
2018-12-03 12:58:25 +0100

    weekday API

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,23 @@
-from flask import Flask
+import datetime
+import calendar
+from flask import Flask, jsonify
+import socket
+
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+@application.route("/weekday/<int:i_year>/<int:i_month>/<int:i_day>")
+def weekday( i_year, i_month, i_day):
+    my_date = datetime.date(i_year, i_month, i_day)
+    ret_weekday = calendar.day_name[my_date.weekday()]
+
+    ret_hostname = socket.gethostname()
+
+    return jsonify( { 'weekdate': ret_weekday, 'worker': ret_hostname } )
 
 if __name__ == "__main__":
     application.run()
